687732199a189c9760eda4a215728813e08d5462
kwon-mingoo-3596 <80897697+kwon-mingoo-3596@users.noreply.github.com>
2022-05-01 22:27:42 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+
+namespace test
+{
+	static class Program
+	{
+		/// <summary>
+		/// 해당 애플리케이션의 주 진입점입니다.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Mat src = new Mat("00000.jpg");
+			Mat gray = new Mat();
+			Mat binary = new Mat();
+
+			Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+			Cv2.Threshold(gray, binary, 150, 255, ThresholdTypes.Binary);
+
+			
+
+			Cv2.ImShow("src", src);
+			Cv2.ImShow("dst", binary);
+			Cv2.WaitKey(0);
+		}
+	}
+}
